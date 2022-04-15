bb837b6b3c8ddac3e37c74fe167c5e97c71bdba2
dj-judd <dj.judd@gmail.com>
2022-04-14 23:33:25 -0500

    deleting API Responses

--- src/main/java/com/promineotech/jeep/controller/JeepOrderController.java
+++ src/main/java/com/promineotech/jeep/controller/JeepOrderController.java
@@ -1,18 +1,11 @@
 package com.promineotech.jeep.controller;
 
-import java.util.List;
-import javax.validation.constraints.Pattern;
-import org.hibernate.validator.constraints.Length;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import com.promineotech.jeep.Constants;
-import com.promineotech.jeep.entity.Jeep;
-import com.promineotech.jeep.entity.JeepModel;
 import com.promineotech.jeep.entity.Order;
 import com.promineotech.jeep.entity.OrderRequest;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -64,12 +57,4 @@ public interface JeepOrderController {
   @GetMapping
   @ResponseStatus(code = HttpStatus.CREATED)
   Order createOrder(@RequestBody OrderRequest orderRequest);
-  // @formatter:on  
 }
-
-//  VIDEO WAY OF DOING IT
-//  @GetMapping
-//  @ResponseStatus(code = HttpStatus.OK)
-//  List<Jeep> fetchJeeps(
-//      @RequestParam(required = false) JeepModel model,
-//      @RequestParam(required = false) String trim);
